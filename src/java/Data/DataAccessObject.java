c3ae69d2cad0b0b39c60967bff91028a7d5129e7
David Carl <David.martin.carl@gmail.com>
2017-02-28 14:48:21 +0100

    Create user SQL call + hashed password + salt
    ez pz

--- src/java/Data/DataAccessObject.java
+++ src/java/Data/DataAccessObject.java
@@ -7,19 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import Data.Password;
 
 public class DataAccessObject {
 
     private DBConnector db = null;
     private Connection conn = null;
-
-    /**
-     * The class constructor gives access to the getters in the class, which
-     * give access to the database. The constructor also opens a connection to
-     * the database.
-     *
-     * @param inputcon is the connection to the database that will be used.
-     */
+    private Password pass = new Password();
+    
     public DataAccessObject(DBConnector inputcon) {
         try {
             db = inputcon;
@@ -28,6 +23,23 @@ public class DataAccessObject {
         } catch (Exception ex) {
             Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public boolean createUser(String uname, String password){
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO users (uname, hashedpw, salt, balance) VALUES (?, ?, ?, ?)";
+        String salt = pass.getSaltString();
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, uname);
+            stmt.setString(2, pass.get_SHA_512_SecurePassword(password, salt));
+            stmt.setString(3, salt);
+            stmt.setInt(4, 100000);
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
     public String getPriceLevel(int pno) {
